3c65f220b89dab4f92bb0b6e1946e4bfd7e9c104
Brandon-Key <bkey@mindex.com>
2024-07-24 10:20:29 -0400

    Added JSON minify and prtty

---
+++ Encodings/JsonMinify.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace BKey.Util.Encode.Encodings;
+
+[Encoder("JsonMinify")]
+public class JsonMinify : IEncoder
+{
+    public string Process(string input)
+    {
+        var jsonDocument = JsonDocument.Parse(input);
+        return JsonSerializer.Serialize(jsonDocument, new JsonSerializerOptions { WriteIndented = false });
+    }
+}
---
+++ Encodings/JsonPrettify.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace BKey.Util.Encode.Encodings;
+
+[Encoder("JsonPrettify")]
+public class JsonPrettify : IEncoder
+{
+    public string Process(string input)
+    {
+        var jsonDocument = JsonDocument.Parse(input);
+        return JsonSerializer.Serialize(jsonDocument, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
