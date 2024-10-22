12c46c9982aef50d8743a9761658a71a2aeacc9e
Brandon-Key <bkey@mindex.com>
2024-10-22 16:05:16 -0400

    Better

--- BKey.Util.Encode.Web/Angular/src/app/encoding/encoding.component.ts
+++ BKey.Util.Encode.Web/Angular/src/app/encoding/encoding.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit} from '@angular/core';
-import { EncodingService } from './encoding.service';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { EncodingService } from './encoding.service';
+
 
 
 @Component({
@@ -15,9 +19,12 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     FormsModule,
-    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatSelectModule,
-    MatIconModule
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule
   ]
 })
 export class EncodingComponent implements OnInit {
